fe3dc0f726fd548fcd8dfcfc25545904273ae81a
maoyuanqi <maoyuanqi330@outlook.com>
2020-08-04 10:44:53 +0800

    updated

--- QDCCBRPA/clientfunctons/rpa_daily_increment.py
+++ QDCCBRPA/clientfunctons/rpa_daily_increment.py
@@ -2,9 +2,11 @@ import tagui as t
 import datetime
 import pandas as pd
 
+
+
 def getdailyincrement(date):
-    
-    
+
+
     #初始化页面
     t.init()
     #输入url进入
@@ -16,41 +18,41 @@ def getdailyincrement(date):
     t.hover(element_identifier='//*[@id="fxr"]')
     t.click(element_identifier='//*[@id="fxr"]')
     t.type(element_identifier='//*[@id="fxr"]',text_to_type=str_to_append)
-    #再点击，确保日期不会遮住底下的搜索按钮
+        #再点击，确保日期不会遮住底下的搜索按钮
     t.click(element_identifier='//*[@id="fxr"]')
     t.hover(element_identifier='//*[@class="ipf01"]')
     t.click(element_identifier='//*[@class="ipf01"]')
     #把展示的尺寸设置为50个产品每页：
     t.hover(element_identifier='//*[@data-pagesize="50"]')
     t.click(element_identifier='//*[@data-pagesize="50"]')
-    
+
     #当下一页没有被disable的时候,有以下超参数
     page_curr = 1 #当前页面index
     value_dict = {} #存放data
     count = 1 #csv 命名用
     #存放列名
     name_list = ['序号', '综合评级', 'url']
-    
+
     for col_name in name_list:
         value_dict.setdefault(col_name, []) #初始化空数据集
 
-    
+
     #当可以翻页，或数据只有一页的时候，进行循环
     while (t.read(element_identifier='//div[@id = "pagefoot"]//a[@class = "cur pf-disabled"]') == str(page_curr)) or (page_curr == 1):
-        
+
         #每页的数据量大小（row number）
         count_values = int(t.count(element_identifier='//tbody[@id = "content"]//tr')) + 1 # python从0开始
         # 爬取页面所有一个table里的值
         if str(t.read(
-                      element_identifier='//tbody[@id = "content"]//tr[' + str(
-                                                                               count_values - 1) + ']//td[@class = "px"]')) > str_to_append:
+                element_identifier='//tbody[@id = "content"]//tr[' + str(
+                            count_values - 1) + ']//td[@class = "px"]')) > str_to_append:
             # print("direct continue..")
             # 翻页
             page_curr += 1
-              # 鼠标模拟移动，并点击翻页
-              t.hover(element_identifier='//*[@href="' + str(page_curr) + '"]')
-              t.click(element_identifier='//*[@href="' + str(page_curr) + '"]')
-              continue
+            # 鼠标模拟移动，并点击翻页
+            t.hover(element_identifier='//*[@href="' + str(page_curr) + '"]')
+            t.click(element_identifier='//*[@href="' + str(page_curr) + '"]')
+            continue
         filename = str(count) + "daily_data.csv"
         count += 1
         t.wait(1)  # 等1秒，万一加载错误了
@@ -59,7 +61,7 @@ def getdailyincrement(date):
             # 判定条件：如果是今天刚发行的，拿到所有主页面上的数据；
             #如果最下面那条数据都大于今天，就直接翻页
             if str(t.read(
-                          element_identifier='//tbody[@id = "content"]//tr[' + str(count_values-1) + ']//td[@class = "px"]')) > str_to_append:
+                    element_identifier='//tbody[@id = "content"]//tr[' + str(count_values-1) + ']//td[@class = "px"]')) > str_to_append:
                 # print("direct break..")
                 break
             else:
@@ -72,7 +74,7 @@ def getdailyincrement(date):
                     value_dict[name_list[2]].append(t.read(element_identifier='//tbody[@id = "content"]//tr['+ str(i) +']//a/@href'))
                 else: #如果不是今天增量，什么都不做
                     pass
-                  # print("turn the page..")
+        # print("turn the page..")
         # 翻页
         page_curr += 1
         # 鼠标模拟移动，并点击翻页
@@ -84,7 +86,7 @@ def getdailyincrement(date):
     #输出格式为："今日日期.csv"
     today_data = pd.DataFrame(value_dict)
     today_data.to_csv(str_to_append+".csv",index=False,encoding='UTF-8')
-    return count
+    return count-1
 
 if __name__ == '__main__':
     # 获取今天（现在时间）
@@ -101,6 +103,7 @@ if __name__ == '__main__':
     final.drop_duplicates(inplace=True)
 
     final = pd.DataFrame(
-                     final.loc[:, ['序号', '产品名称', '发行银行', '委托货币', '发行日', '停售日', '管理期(天)', '预期收益率', '到期收益率', '与同期储蓄比', '综合评级_x','url']])
+        final.loc[:, ['序号', '产品名称', '发行银行', '委托货币', '发行日', '停售日', '管理期(天)', '预期收益率', '到期收益率', '与同期储蓄比', '综合评级_x',
+                      'url']])
     final.rename(columns={'综合评级_x': '综合评级'}, inplace=True)
     final.to_csv(str_to_append+"final.csv", encoding='UTF-8', index=False)
