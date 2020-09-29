12ded6d3b23d6eda7cdc5a8469a7f8b2d9613c4b
maoyuanqi <31228720+maoyuanqi@users.noreply.github.com>
2020-09-29 08:08:31 +0000

    20200929 新需求

---
+++ product_daily_report.py
@@ -0,0 +1,30 @@
+import tagui as t
+import datetime
+import pandas as pd
+import os
+import s3_function
+
+t.init()
+t.wait(5)
+#read page
+t.url("http://bank.jrj.com.cn/bankpro/")
+
+#locate items
+item_list = t.read(element_identifier='//div[@class = "qutop mt"]').split()
+
+#txt
+today = datetime.datetime.today()
+today = str(today.date())
+file_name = today+'product_daily.txt'
+with open(file_name,'w',encoding='utf-8') as f:
+    f.writelines([item+'\n' for item in item_list])
+#excel
+# daily_on_sale = int(item_list[1][:-11])
+# monthly_on_sale =int(item_list[2][:-13])
+# monthly_expire = int(item_list[3][:-13])
+#
+# pd.DataFrame({'今日在售银行理财产品':int(daily_on_sale),'本月累计发售银行理财产品':int(monthly_on_sale),'本月累计到期银行理财产品':int(monthly_expire)},index=[0]).to_csv(today+'product_daily.csv',index = False, encoding = 'utf-8')
+s3_function.upload_to_aws_s3(file_name,'s3qingdao',file_name)
+t.close()
+
+
